313601640e5b84bf63cde7d381cdfc96b08c0d55
Gabriel Cesar <gcal1@live.com>
2017-07-18 14:50:27 -0300

    formatando para ES6

--- src/main/resources/static/layout/js/filtro.js
+++ src/main/resources/static/layout/js/filtro.js
@@ -21,22 +21,23 @@ $(() => {
 })
 
 function mostrarProjetos(projetos) {
-    for (var pos in projetos) {
-        var pedido = projetos[pos]
-        $('#projetos').append(
-            '<div class="divider col m12 s12"></div>' +
-            '<div class="col m12 s12">' +
-                '<h4 class="blue-text left">' + pedido.nome + '</h4>' +
-                '<a href="/pedidos/user/' + pedido.id + '" class="btn blue right distance-min">Detalhes</a>' +
-            '</div>' +
-            '<div class="col m6 s12">' +
-                '<p><strong>Categoria: </strong><span>' + pedido.servico.categoria + '</span></p>' +
-                '<p><strong>Patrocinado: </strong><span>'  + pedido.usuario.nome + '</span></p>' +
-                '<p><strong>Preço estimado: </strong><span>' +  pedido.valorEstimado + '</span></p>' +
-            '</div>' +
-            '<div class="col m6 s12">' +
-                '<p class="truncate">' + pedido.servico.atividade + '</p>' +
-            '</div>'
-        );
-    }
+    projetos.forEach((pedido) => {
+        $('#projetos').append(componentProjeto(pedido))
+    })
+}
+
+function componentProjeto(pedido) {
+    return  `<div class="divider col m12 s12"></div>
+            <div class="col m12 s12">
+            <h4 class="blue-text left">${pedido.nome}</h4>
+            <a href="/pedidos/user/${pedido.id}" class="btn blue right distance-min">Detalhes</a>
+            </div>
+            <div class="col m6 s12">
+            <p><strong>Categoria: </strong><span>${pedido.servico.categoria}</span></p>
+            <p><strong>Patrocinado: </strong><span>${pedido.usuario.nome}</span></p>
+            <p><strong>Preço estimado: </strong><span>${pedido.valorEstimado}</span></p>
+            </div>
+            <div class="col m6 s12">
+            <p class="truncate">${pedido.servico.atividade}</p>
+            </div>`
 }
